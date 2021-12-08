2bf6eb1543ae3c0bfaf329bd4d6f7a2e7c4e946c
Jonathan Frere <jonathan.frere@gmail.com>
2021-12-08 19:28:48 +0100

    Add part two code

--- day8/day8.ml
+++ day8/day8.ml
@@ -1,88 +1,143 @@
-module Display = struct
-  type t = {
-    top : bool;
-    top_left : bool;
-    top_right : bool;
-    middle : bool;
-    bottom_left : bool;
-    bottom_right : bool;
-    bottom : bool;
-  }
+let explode s = List.init (String.length s) (String.get s)
 
-  let blank =
-    {
-      top = false;
-      top_left = false;
-      top_right = false;
-      middle = false;
-      bottom_left = false;
-      bottom_right = false;
-      bottom = false;
-    }
+let sort_string s =
+  s |> String.to_seq |> List.of_seq |> List.sort compare |> List.to_seq
+  |> String.of_seq
 
-  let full =
-    {
-      top = true;
-      top_left = true;
-      top_right = true;
-      middle = true;
-      bottom_left = true;
-      bottom_right = true;
-      bottom = true;
-    }
+module DisplayPart = struct
+  type t =
+    | Top
+    | TopLeft
+    | TopRight
+    | Middle
+    | BottomLeft
+    | BottomRight
+    | Bottom
 
-  let of_int i =
-    match i with
-    | 0 -> { full with middle = false }
-    | 1 -> { blank with top_right = true; bottom_right = true }
-    | 2 -> { full with top_left = false; bottom_right = false }
-    | 3 -> { full with top_left = false; bottom_left = false }
-    | 4 -> { full with top = false; bottom_left = false; bottom = false }
-    | 5 -> { full with top_right = false; bottom_left = false }
-    | 6 -> { full with top_right = false }
-    | 7 -> { blank with top = true; top_right = true; bottom_right = true }
-    | 8 -> full
-    | 9 -> { full with bottom_left = false }
-    | _ -> failwith "i must be a single digit"
+  let compare = compare
 
-  let count_segments display =
-    Bool.to_int display.top
-    + Bool.to_int display.top_left
-    + Bool.to_int display.top_right
-    + Bool.to_int display.middle
-    + Bool.to_int display.bottom_left
-    + Bool.to_int display.bottom_right
-    + Bool.to_int display.bottom
+  let to_string = function
+    | Top -> "Top"
+    | TopLeft -> "TopLeft"
+    | TopRight -> "TopRight"
+    | Middle -> "Middle"
+    | BottomLeft -> "BottomLeft"
+    | BottomRight -> "BottomRight"
+    | Bottom -> "Bottom"
 end
 
-module Solver = struct
-  type guess = Solved of int | Narrowed of int list | Unsolved
+module DisplayPartMap = Map.Make (DisplayPart)
 
-  let solve_by_length case =
+module Display = struct
+  include DisplayPartMap
+
+  let create value =
+    DisplayPartMap.add_seq
+      (List.to_seq
+         [
+           (DisplayPart.Top, value);
+           (DisplayPart.TopLeft, value);
+           (DisplayPart.TopRight, value);
+           (DisplayPart.Middle, value);
+           (DisplayPart.BottomLeft, value);
+           (DisplayPart.BottomRight, value);
+           (DisplayPart.Bottom, value);
+         ])
+      DisplayPartMap.empty
+end
+
+module Decoder = struct
+  type partial = { parts : char list Display.t; known : (string * int) list }
+
+  let unsolved =
+    { parts = Display.create [ 'a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g' ]; known = [] }
+
+  let solved { known; parts = _ } = List.length known == 10
+
+  let deduce_by_length { parts; known } case =
+    let remove_chars existing_chars chars =
+      List.filter (fun x -> not (List.mem x chars)) existing_chars
+    in
+    let refine positions chars p =
+      Display.mapi
+        (fun part last_chars ->
+          if List.mem part positions then
+            List.filter (fun x -> List.mem x chars) last_chars
+          else remove_chars last_chars chars)
+        p
+    in
+
     match String.length case with
-    | 2 -> Solved 1
-    | 3 -> Solved 7
-    | 4 -> Solved 4
-    | 5 -> Narrowed [ 2; 3; 5 ]
-    | 6 -> Narrowed [ 6; 9; 0 ]
-    | 7 -> Solved 8
+    | 2 ->
+        {
+          parts =
+            refine DisplayPart.[ TopRight; BottomRight ] (explode case) parts;
+          known = (case, 1) :: known;
+        }
+    | 3 ->
+        {
+          parts =
+            refine
+              DisplayPart.[ Top; TopRight; BottomRight ]
+              (explode case) parts;
+          known = (case, 7) :: known;
+        }
+    | 4 ->
+        {
+          parts =
+            refine
+              DisplayPart.[ TopLeft; TopRight; Middle; BottomRight ]
+              (explode case) parts;
+          known = (case, 4) :: known;
+        }
+    | 5 | 6 -> { parts; known }
+    | 7 -> { parts; known = (case, 8) :: known }
     | n ->
         failwith (Printf.sprintf "impossible length of string (%d, %s)" n case)
+
+  let deduce_by_parts { parts; known } case =
+    let try_parts to_find =
+      let search_chars =
+        List.concat_map (fun part -> Display.find part parts) to_find
+      in
+      List.for_all (String.contains case) search_chars
+    in
+    if List.mem_assoc case known then { parts; known }
+    else if try_parts DisplayPart.[ Bottom; TopRight ] then
+      { parts; known = (case, 0) :: known }
+    else if try_parts DisplayPart.[ Middle; TopRight ] then
+      { parts; known = (case, 9) :: known }
+    else if try_parts DisplayPart.[ Middle; Bottom ] then
+      { parts; known = (case, 6) :: known }
+    else if try_parts DisplayPart.[ Bottom ] then
+      { parts; known = (case, 2) :: known }
+    else if try_parts DisplayPart.[ Middle ] then
+      { parts; known = (case, 5) :: known }
+    else if try_parts DisplayPart.[ TopRight ] then
+      { parts; known = (case, 3) :: known }
+    else failwith (Printf.sprintf "unknown case %s" case)
+
+  let of_digits clues =
+    let found = List.fold_left deduce_by_length unsolved clues in
+    let found = List.fold_left deduce_by_parts found clues in
+    found
+
+  let decode decoder s =
+    let result = List.assoc s decoder.known in
+    result
 end
 
 module Input = struct
   type t = { digits : string list; code : string list }
 
-  let create digits code = { digits; code }
+  let create digits code =
+    { digits = List.map sort_string digits; code = List.map sort_string code }
 
   let parse input =
     let split_parts str = str |> String.trim |> String.split_on_char ' ' in
     match String.split_on_char '|' input with
-    | [ digits; code ] ->
-        { digits = split_parts digits; code = split_parts code }
+    | [ digits; code ] -> create (split_parts digits) (split_parts code)
     | _ -> failwith "invalid input format"
-
-  let find_easy_cases input = List.map Solver.solve_by_length input.code
 end
 
 let input filename =
@@ -96,13 +151,30 @@ let input filename =
   result := List.rev !result;
   List.map Input.parse !result
 
-let find_easy_cases input =
+let find_easy_cases (input : Input.t list) =
+  let find_easy_case decoder input =
+    match Decoder.decode decoder input with 1 | 4 | 7 | 8 -> 1 | _ -> 0
+  in
   input
-  |> List.map Input.find_easy_cases
-  |> List.flatten
+  |> List.map (fun i -> (i, Decoder.of_digits i.Input.digits))
   |> List.fold_left
-       (fun acc guess -> acc + match guess with Solver.Solved _ -> 1 | _ -> 0)
+       (fun acc (input, decoder) ->
+         acc
+         + List.fold_left
+             (fun acc code -> acc + find_easy_case decoder code)
+             0 input.Input.code)
        0
+
+let find_full_results (input : Input.t list) =
+  let find_case decoder input =
+    List.fold_left2
+      (fun acc s i -> acc + (i * Decoder.decode decoder s))
+      0 input.Input.code [ 1000; 100; 10; 1 ]
+  in
+  input
+  |> List.map (fun i -> (i, Decoder.of_digits i.Input.digits))
+  |> List.map (fun (i, decoder) -> find_case decoder i)
+  |> List.fold_left ( + ) 0
 
 let demo_input =
   [
@@ -260,11 +332,11 @@ let () =
           test_case "Real (1)" `Quick (fun () ->
               let initial = input "inputs/day8.txt" in
               (check int) "" 449 (find_easy_cases initial));
-          (* test_case "Example (2)" `Quick (fun () ->
-                 let initial = [ 16; 1; 2; 0; 4; 2; 7; 1; 2; 14 ] in
-                 (check int) "" 168 (naive Crab.move_2 initial));
-             test_case "Real (2)" `Quick (fun () ->
-                 let initial = input "inputs/day7.txt" in
-                 (check int) "" 0 (naive Crab.move_2 initial)); *)
+          test_case "Example (2)" `Quick (fun () ->
+              let initial = demo_input in
+              (check int) "" 61229 (find_full_results initial));
+          test_case "Example (2)" `Quick (fun () ->
+              let initial = input "inputs/day8.txt" in
+              (check int) "" 61229 (find_full_results initial));
         ] );
     ]
